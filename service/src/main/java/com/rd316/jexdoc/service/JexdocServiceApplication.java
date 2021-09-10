a9ff7e0747623fc14568aecc460889cb1d1f1f99
rd316-dev <rd316.gortor20@gmail.com>
2021-09-10 16:08:26 +0300

    Java Document Exchange initial commit
    Signed-off-by: Ruslan Abzalov <rd316.gortor20@gmail.com>

---
+++ service/src/main/java/com/rd316/jexdoc/service/JexdocServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rd316.jexdoc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JexdocServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JexdocServiceApplication.class, args);
+    }
+
+}
